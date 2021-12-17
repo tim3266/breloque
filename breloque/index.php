f2a9fc08d1ca4c1ad158da2ad7aaa3290572e7e0
tim <tim.gaillard@gmail.com>
2021-12-17 10:38:03 +0100

    ajout google analytics

--- breloque/index.php
+++ breloque/index.php
@@ -4,6 +4,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-3MW6R7QH7Z"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-3MW6R7QH7Z');
+    </script>
 </head>
 <body>
 <?php
@@ -15,8 +24,6 @@ echo "maj 2";
 <a href="javascript:openAxeptioCookies()">
   Cliquez-ici pour modifier vos préférences en matière de cookies
 </a>
-</body>
-</html>
 <script>
 window.axeptioSettings = {
   clientId: "61bc377a12dcbf3b9b6e6c81",
@@ -29,3 +36,5 @@ window.axeptioSettings = {
   t.parentNode.insertBefore(e, t);
 })(document, "script");
 </script>
+</body>
+</html>
