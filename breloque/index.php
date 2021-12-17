6f46f339184c741f63d45ab80d1ff0da8ce694a0
tim <tim.gaillard@gmail.com>
2021-12-17 08:29:44 +0100

    gestion cookies début

--- breloque/index.php
+++ breloque/index.php
@@ -1,5 +1,27 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
+
 echo "test";
 echo "maj 2";
-a
-?>
+?>   
+</body>
+</html>
+<script>
+window.axeptioSettings = {
+  clientId: "61bc377a12dcbf3b9b6e6c81",
+  cookiesVersion: "breloque-base",
+};
+ 
+(function(d, s) {
+  var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+  e.async = true; e.src = "//static.axept.io/sdk.js";
+  t.parentNode.insertBefore(e, t);
+})(document, "script");
+</script>
