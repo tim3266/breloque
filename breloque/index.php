de923b9cbf4638d977f528563d3984ccadef556a
tim <tim.gaillard@gmail.com>
2021-12-17 12:06:06 +0100

    axeptio integration cookie google analytics

--- breloque/index.php
+++ breloque/index.php
@@ -7,11 +7,28 @@
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-3MW6R7QH7Z"></script>
     <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
 
-    gtag('config', 'G-3MW6R7QH7Z');
+    function launchGA(){
+        var el = document.createElement('script');
+        el.setAttribute('type', 'text/javascript');
+        el.setAttribute('type', true);
+        el.setAttribute('src', '//www.googletagmanager.com/gtag/js?id=G-3MW6R7QH7Z');
+        document.body.append(el);window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js',new Date());
+        gtag('config','', {'anonymize_ip':true});
+        gtag('config','', {'link_attribution':true});
+    }
+    
+    // La fonction qui relie Axeptio au lancement du cookie Google Analytics
+    void 0 === window._axcb && (window._axcb = []);
+    window._axcb.push(function(axeptio) {
+        axeptio.on("cookies:complete", function(choices) {
+    if(choices.google_analytics) {
+       launchGA();
+      }
+     });
+});
     </script>
 </head>
 <body>
